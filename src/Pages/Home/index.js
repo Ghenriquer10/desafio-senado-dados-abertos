7a693a19f8522a9bc7b607d9c8db821b8e41fd40
Gustavo Henrique <ghenriquer10@gmail.com>
2022-08-22 17:53:45 -0300

    adding search field styling

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react';
+import * as C from './style';
+import {FiSearch} from 'react-icons/fi'
 
 export default function Home(){
+
+    const [inputSearch, setInputSearch] = useState(false)
+
+    function setInputVisible(){
+        setInputSearch(prev => !prev)
+    }
+
     return(
-        <>
-            <h1>Home</h1>
-        </>
+        <C.Container>
+            <C.SearchField>
+                <C.SearchAbout>
+                    <p>Dados Abertos</p>
+                    <div className='input-field'>
+                        <input placeholder='Buscar'/>
+                        <FiSearch size={20} className={'web-search'}/>
+                        <FiSearch size={20} className={'mobile-search'} onClick={setInputVisible}/>
+                    </div>
+                </C.SearchAbout>
+                {inputSearch && (
+                    <C.MobileInput>
+                        <input placeholder='Buscar'/>
+                    </C.MobileInput>
+                )}
+                <C.PageAbout>
+                    <p>Senadores</p>
+                </C.PageAbout>
+            </C.SearchField>
+        </C.Container>
     )
 }
---
+++ src/Pages/Home/style.js
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    width: 100%;
+`;
+
+export const SearchField = styled.div`
+    height: 130px;
+    background-color: #0095DA;
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    gap: 7px;
+`;
+
+export const SearchAbout = styled.div`
+    width: 100%;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    p{
+        font-size: 2.5em;
+    }
+
+        .input-field{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+    
+            input{
+                height: 40px;
+                width: 230px;
+                padding: 0 15px;
+                background-color: #005B9E;
+                color: #fff;
+                border: none;
+    
+                @media (min-width: 320px) and (max-width: 480px) {
+                    display: none;
+                }
+    
+    
+            }
+    
+            input::placeholder{
+                color: #CCCCCC;
+                font-size: 1.3em;
+            }
+    
+            svg.web-search{
+                background-color: #005B9E;
+                height: 40px;
+                width: 40px;
+                padding: 10px;
+
+                @media (min-width: 320px) and (max-width: 480px) {
+                    display: none;
+                }
+            }
+
+            svg.mobile-search{
+                background-color: #0095DA;
+                height: 40px;
+                width: 40px;
+                padding: 10px;
+
+                @media (min-width: 481px) {
+                    display: none;
+                }
+            }
+        }
+`;
+
+export const MobileInput = styled.div`
+    width: 100%;
+        input{
+            width: 100%;
+            height: 35px;
+        }
+`;
+
+export const PageAbout = styled.div`
+    display: flex;
+    align-items: flex-start;
+    width: 100%;
+    color: #ffffff;
+    line-height: 1.5;
+    font-weight: 400;
+    font-size: 1.3em;
+`;
